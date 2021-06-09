fe9c07028de3ef31822e44afcb077433109c2887
Hannes Achleitner <hannes.achleitner@partner.bmw.de>
2021-06-09 08:23:02 +0200

    fixup! Publish with tag name

--- uploadAllMaven.sh
+++ uploadAllMaven.sh
@@ -1,7 +1,18 @@
-# iterate all project to upload, indicated by "uploadArchives" task
+#!/bin/zsh
 
+find . -name "*.aar"
+
+# iterate all project to upload, indicated by "uploadArchives" task
 grep uploadArchives ./platforms -lR | xargs -n1 dirname | while read -r module ; do
-   pushd $module
-   gradle uploadArchives
-   popd 1>/dev/null
+   echo "cd $module && gradle uploadArchives && cd ../../../.."
+done
+
+grep uploadArchives ./platforms -lR | xargs -n1 dirname > temp.list
+
+cat temp.list | while read -r module ; do
+   cd $module
+   echo "I'm in $(pwd)"
+   gradle uploadArchives | echo "all done"
+   set -x
+   cd ../../../..
 done
